7e61a0132a3042af374f43765105594ae0ba6e92
Tyler <tyler@pinkrobotics.ca>
2020-11-09 19:09:17 -0800

    increased parallelism in training set creators

--- 2_models/model.sh
+++ 2_models/model.sh
@@ -6,8 +6,9 @@ age=`cat $root/../data/state/age`
 echo -n "| model: "
 if [[ ${age} -gt 2 ]]
 then
-  find ../data/raw/     -type f -print0 | xargs -0 -n 100 -P 8 ./create_training.sh 
-  find ../data/windows/ -type f -print0 | xargs -0 -n 100 -P 8 ./create_labels.sh 
+  find ../data/raw/     -type f -print0 | xargs -0 -n 50 -P 16 ./create_training.sh 
+  find ../data/windows/ -type f -print0 | xargs -0 -n 100 -P 16 ./create_labels.sh 
+  wait
   echo -n -e " \033[;31m\u2713\033[0m "  
 else
   echo -n -e " \033[;36mX\033[0m "
