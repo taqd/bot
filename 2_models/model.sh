357f3f38a1e15fc25e5154d5af1d4deacca2d822
Tyler <tyler@pinkrobotics.ca>
2020-11-10 11:32:28 -0800

    @

--- 2_models/model.sh
+++ 2_models/model.sh
@@ -9,9 +9,10 @@ then
   find ../data/raw/     -type f -print0 | xargs -0 -n 10 -P 32 ./create_training.sh & 
   find ../data/windows/ -type f -print0 | xargs -0 -n 100 -P 16 ./create_labels.sh &
   wait
-  find ../data/training/ -name '*_data.csv' -type f -print0 | xargs -0 -n 10 -P 2 \
+  find ../data/training/ -name '*_data.csv' -type f -print0 | xargs -0 -n 50 -P 16 \
     ./normalize.sh &
   wait
+  
   echo -n -e " \033[;31m\u2713\033[0m "  
 else
   echo -n -e " \033[;36mX\033[0m "
--- 2_models/normalize.sh
+++ 2_models/normalize.sh
@@ -4,8 +4,30 @@ root=~/bot/2_models
 for file in "$@"
 do
   name=${file##*/}
-  mlpack_preprocess_scale -i $file -o ../data/modeling/${name}_normed.csv \
+  name=${name%_data.*}
+  # tail -n -10 data | sed 's/nan/0/g' > ../data/modeling/${name}_tmp.csv
+  # head -n -10 targets > ../data/modeling/${target}_${window}
+  # normalize targets
+
+  datafile=$file
+  label10=$root/../data/training/${name}_10_label.csv
+  label60=$root/../data/training/${name}_60_label.csv
+  label1440=$root/../data/training/${name}_1440_label.csv
+  label10080=$root/../data/training/${name}_10080_label.csv
+
+  mlpack_preprocess_scale -i $datafile -o ../data/modeling/${name}_normed.csv \
       -a min_max_scaler -e 2 -b -2
+  mlpack_preprocess_scale -i $label10 -o ../data/modeling/${name}_normed.csv \
+      -a min_max_scaler -e 2 -b -2
+  mlpack_preprocess_scale -i $label60 -o ../data/modeling/${name}_normed.csv \
+      -a min_max_scaler -e 2 -b -2
+  mlpack_preprocess_scale -i $label1440 -o ../data/modeling/${name}_normed.csv \
+      -a min_max_scaler -e 2 -b -2
+  mlpack_preprocess_scale -i $label10080 -o ../data/modeling/${name}_normed.csv \
+      -a min_max_scaler -e 2 -b -2
+
+
+
 
   #mlpack_perceptron -l ../data/training/XXBTZUSD_tick_askprice_10_labels.csv -t \
   #  ../data/modeling/${name}_normed.csv -T ../data/modeling/${name}_normed.csv -P \
