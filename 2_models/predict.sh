0346498c8e0acb56c20b25aac5585ca5786fb5cf
Tyler <tyler@pinkrobotics.ca>
2020-11-14 19:32:20 -0800

    @

--- 2_models/predict.sh
+++ 2_models/predict.sh
@@ -14,16 +14,14 @@ train_norm=$root/../data/modeling/${name}_train_norm.csv
 labels_norm=$root/../data/modeling/${name}_labels_norm.csv
 testing_norm=$root/../data/modeling/${name}_test_norm.csv
 
-norm_model=$root/../data/modeling/${name}_norm_model.txt
-perc1_model=$root/../data/modeling/${name}_perc1_model.txt
+norm_model=$root/../data/modeling/${name}_norm_model.json
+perc1_model=$root/../data/modeling/${name}_perc1_model.json
 
 perc1_preds=$root/../data/predictions/${name}_perc1.csv
 
 window=${datum_file##*_}
 let windowplus1=window+1
 
-#echo "datum file: ${window}"
-#exit
 let training_size=window*2
 tail -n $training_size $datums > ${datums}_tmp
 mv ${datums}_tmp $datums
@@ -37,7 +35,7 @@ tail -n +${windowplus1} $targets > $labels
 
 if [[ $train_size -gt 0 ]]
 then
-  
+
   mlpack_preprocess_scale -a min_max_scaler -e 2 -b -2 \
     --input_file $train                                \
     --output_file $train_norm                          \
@@ -53,12 +51,24 @@ then
     --input_file $testing                              \
     --output_file $testing_norm 
 
-  mlpack_perceptron --max_iterations 1000 \
-    --labels_file $labels_norm            \
-    --training_file $train_norm           \
-    --test_file $testing_norm             \
-    --predictions_file $perc1_preds       \
-    --output_model_file $perc1_model
+  if [[ ! -f $perc1_model ]]
+  then
+    mlpack_perceptron --max_iterations 10000 \
+      --labels_file $labels_norm            \
+      --training_file $train_norm           \
+      --test_file $testing_norm             \
+      --predictions_file $perc1_preds       \
+      --output_model_file $perc1_model
+
+  else
+    mlpack_perceptron --max_iterations 10000 \
+      --input_model_file $perc1_model       \
+      --labels_file $labels_norm            \
+      --training_file $train_norm           \
+      --test_file $testing_norm             \
+      --predictions_file $perc1_preds       \
+      --output_model_file $perc1_model
+  fi
 
 fi
 
