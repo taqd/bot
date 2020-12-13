edc1197fe6a877ff3e47e02804e8fc02ae2d3dab
taqd <tyler@pinkrobotics.ca>
2020-12-12 20:10:42 -0800

    widening

--- 0_download/download_all.sh
+++ 0_download/download_all.sh
@@ -11,8 +11,20 @@ function go_b {
   echo -n "*"
 }
 
+function go_c {
+  ssh  -i /home/tyler/.ssh/google_compute_engine tyler@35.226.248.3 'cd bot; make run' 
+  echo -n "*"
+}
+
+function go_d {
+  ssh  -i /home/tyler/.ssh/google_compute_engine tyler@34.125.85.148 'cd bot; make run' 
+  echo -n "*"
+}
+
 echo -n " | downloading all:"
 go_a &
 go_b &
+go_c &
+go_d &
 wait
 echo " *"
