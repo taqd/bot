a5317050e3d5c449eb76f79f99c45a31e93ed65f
tyler <tyler@taqd.ca>
2020-12-14 19:46:05 +0000

    @

---
+++ 3_output/start_4k.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+root=~/bot/3_output
+data=$root/../data
+
+YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2"
+
+SIZE="3840x2160"
+YOUTUBE_KEY="ggz6-cwcd-2aq6-xk64-dsgg"
+VIDEO_SOURCE="${data}/state/output_4k_black.bmp"
+  
+ffmpeg \
+ -stream_loop -1 \
+ -f image2 \
+ -s $SIZE \
+ -i "$VIDEO_SOURCE" \
+ -re \
+ -f lavfi \
+ -i anullsrc \
+ -c:v libx264 \
+ -g 25 \
+ -x264opts no-scenecut \
+ -preset ultrafast \
+ -tune stillimage \
+ -crf 0 \
+ -pix_fmt yuv420p \
+ -r 30 \
+ -f flv \
+ $YOUTUBE_URL/$YOUTUBE_KEY
+ 
